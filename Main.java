0c8611b4afa74ab4ba4c4afa754277ab7d88edfb
Tomaev <Tomaev1999@internet.ru>
2024-07-27 17:08:34 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static String value1;
+    static ArrayList<String> value1Array = new ArrayList<>();
+    static
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String sc = scanner.nextLine();
+        calc(sc);
+
+    }
+    static void calc(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            for (int count = i + 1; count < input.length(); count++) {
+                if(value1 == null && ch == '"'){
+
+                }
+            }
+            String[]
+        }
+    }
+}
